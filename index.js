18a8d2027263998016f453cce116bcb0502dd9a3
bsef19m012 <>
2021-07-04 15:02:11 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// document.getElementById("count").innerText = 5
+
+// change the count-el in the HTML to reflect the new count
+let count = 0
+
+function increment() {
+    console.log("The button was clicked")
+    count = count + 1
+    document.getElementById("count-el").innerText = count
+}
+
+
